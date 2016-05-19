f09c5c933bc97fdeff92320ba7f2bd22f16c382d
elili <eli@crownpartners.com>
2016-05-19 18:38:16 +0800

    package war

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,12 +2,26 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application
+public class Application extends SpringBootServletInitializer
 {
+    /**
+     * The first step in producing a deployable war file is to provide a SpringBootServletInitializer subclass and override its configure method.
+     * This makes use of Spring Framework’s Servlet 3.0 support and allows you to configure your application when it’s launched by the servlet container.
+     * Typically, you update your application’s main class to extend SpringBootServletInitializer
+     * @param application
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(Application.class);
+    }
 
-	public static void main(String[] args) 
+    public static void main(String[] args)
 	{
         SpringApplication.run(Application.class, args);
     }
