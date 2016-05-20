cc8e6597dfca60abe1e5a73efd482cac82e370e5
elili <eli@crownpartners.com>
2016-05-20 15:18:37 +0800

    master branch had already support  annotation to operate myBatis, but  i still like  the way with xml. Add support xml

--- src/main/java/hello/controller/HelloRest.java
+++ src/main/java/hello/controller/HelloRest.java
@@ -3,7 +3,6 @@ package hello.controller;
 import hello.domain.City;
 import hello.mapper.CityMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,27 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRest
 {
     @Autowired
-    private CityMapper cityMapper;
+    public CityMapper cityMapper;
 
-    @RequestMapping("/findByState")
-    public City findByState(@RequestParam(value = "name", required = false) String name)
+    @RequestMapping("/selectCityByName")
+    public City selectCityByName(@RequestParam(value = "name", required = false) String name)
     {
-        return this.cityMapper.findByState("CA");
+        return this.cityMapper.selectCityByName(name);
     }
 
-    @RequestMapping("/saveCity")
-    public void saveCity(@RequestBody City city)
-    {
-       this.cityMapper.saveCity(city.getName(),city.getState(),city.getCountry());
-    }
-    @RequestMapping("/deleteCity")
-    public void deleteCity(@RequestParam(value = "name", required = true) String name)
-    {
-        this.cityMapper.deleteCity(name);
-    }
-    @RequestMapping("/updateCity")
-    public void updateCity(@RequestParam(value = "name", required = true) String name, @RequestParam(value = "country", required = true) String country)
-    {
-      this.cityMapper.updateCity(name,country);
-    }
 }
