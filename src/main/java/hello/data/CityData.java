e844f7a0f66b2b702aefbf1e6aacd3d47ad1cd24
elili <eli@crownpartners.com>
2016-06-03 18:10:15 +0800

    add data for presentation layer

---
+++ src/main/java/hello/data/CityData.java
@@ -0,0 +1,37 @@
+package hello.data;
+
+/**
+ * Created by elili on 6/3/2016.
+ */
+public class CityData
+{
+    private String name;
+
+    private String state;
+
+    private String country;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
